600b6b4ad4fc0f7efa1948ace2fee394e0aeab2f
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-03-14 21:31:51 +0530

    Update help.py

--- ChampuMusic/plugins/bot/help.py
+++ ChampuMusic/plugins/bot/help.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 import re
 from math import ceil
 from typing import Union
@@ -17,10 +8,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import config
 from config import BANNED_USERS, START_IMG_URL
 from strings import get_command, get_string
-from VIPMUSIC import HELPABLE, app
-from VIPMUSIC.utils.database import get_lang, is_commanddelete_on
-from VIPMUSIC.utils.decorators.language import LanguageStart
-from VIPMUSIC.utils.inline.help import private_help_panel
+from ChampuMusic import HELPABLE, app
+from ChampuMusic.utils.database import get_lang, is_commanddelete_on
+from ChampuMusic.utils.decorators.language import LanguageStart
+from ChampuMusic.utils.inline.help import private_help_panel
 
 ### Command
 HELP_COMMAND = get_command("HELP_COMMAND")
