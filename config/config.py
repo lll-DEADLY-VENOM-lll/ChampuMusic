168a410011bec7978c598d331492db1e374ad9cf
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-03-15 07:18:21 +0530

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -1,156 +1,91 @@
 import re
 from os import getenv
-
 from dotenv import load_dotenv
 from pyrogram import filters
 
+# .env file se variables load karne ke liye
 load_dotenv()
 
-
-# Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID", ""))
+# --- BASIC BOT SETTINGS ---
+# my.telegram.org se API_ID aur API_HASH lein
+API_ID = int(getenv("API_ID", "1234567")) 
 API_HASH = getenv("API_HASH", "")
 
-## Get it from @Botfather in Telegram.
+# @BotFather se liya gaya Bot Token
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
-# Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "aera_music_bot")
+# Aapke Bot ka Username
+BOT_USERNAME = getenv("BOT_USERNAME", "aera_music_bot")
 
-# Get your mongo url from cloud.mongodb.com
+# MongoDB ka URL (cloud.mongodb.com)
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", 5)
-)  # Remember to give value in Seconds
+# --- LIMITS & DURATIONS ---
+# Messages delete hone ka time (Seconds mein)
+CLEANMODE_DELETE_MINS = int(getenv("CLEANMODE_MINS", 5)) 
 
-# Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
-DURATION_LIMIT_MIN = int(
-    getenv("DURATION_LIMIT", 300)
-)  # Remember to give value in Minutes
+# Music chalne ki maximum limit (Minutes mein)
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300)) 
 
+# Download karne ki limit (Minutes mein)
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", 1000))
 
-EXTRA_PLUGINS = getenv(
-    "EXTRA_PLUGINS",
-    True,
-)
-
-# Fill True if you want to load extra plugins
-# Fill here the external plugins repo where plugins that you want to load
-EXTRA_PLUGINS_REPO = getenv(
-    "EXTRA_PLUGINS_REPO",
-    "https://github.com/lll-DEADLY-VENOM-lll/Extra_Plugin",
-)
-
-# Your folder name in your extra plugins repo where all plugins stored
+# --- PLUGINS SETTINGS ---
+EXTRA_PLUGINS = getenv("EXTRA_PLUGINS", True)
+EXTRA_PLUGINS_REPO = getenv("EXTRA_PLUGINS_REPO", "https://github.com/lll-DEADLY-VENOM-lll/Extra_Plugin")
 EXTRA_PLUGINS_FOLDER = getenv("EXTRA_PLUGINS_FOLDER", "plugins")
 
-LOGGERS = "\x54\x68\x65\x54\x65\x6C\x65\x67\x72\x61\x6D\x52\x6F\x62\x6F\x74"  # connect errors api key "Dont change it"
-# Duration Limit for downloading Songs in MP3 or MP4 format from bot
-SONG_DOWNLOAD_DURATION = int(
-    getenv("SONG_DOWNLOAD_DURATION_LIMIT", 1000)
-)  # Remember to give value in Minutes
-
-# You'll need a Private Group ID for this.
+# --- OWNER & LOGGERS ---
+LOGGERS = "\x54\x68\x65\x54\x65\x6C\x65\x67\x72\x61\x6D\x52\x6F\x62\x6F\x74"
 LOGGER_ID = int(getenv("LOGGER_ID", "-1003034048678"))
 
-# Your User ID.
-OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "7755751427").split())
-)  # Input type must be interger
+# Bot Owner ki ID (Space dekar multiple IDs daal sakte hain)
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7755751427").split()))
 
-## Fill these variables if you're deploying on heroku.
-# Your heroku app name
+# --- HEROKU & UPSTREAM REPO ---
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-# Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/TANYA-SINGH-VNS-UP/NO1",
-)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TANYA-SINGH-VNS-UP/NO1")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
-
-# Only  Links formats are  accepted for this Var value.
-SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/heroku_club"
-)  # Example:- https://t.me/akaChampu
-SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/NOBITA_SUPPORT"
-)  # Example:- https://t.me/TheChampuClub
-# Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", True)
-
-# Time after which you're assistant account will leave chats automatically.
-AUTO_LEAVE_ASSISTANT_TIME = int(
-    getenv("ASSISTANT_LEAVE_TIME", 1800)
-)  # Remember to give value in Seconds
-
-# Set it true if you want your bot to be private only [You'll need to allow CHAT_ID via /authorize command then only your bot will play music in that chat.]
-PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", False)
-
-
-# Time sleep duration For Youtube Downloader
-YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", 3))
-
-# Time sleep duration For Telegram Downloader
-TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", 5))
-
-
-# Your Github Repo.. Will be shown on /start Command
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/TANYA-SINGH-VNS-UP/NO1")
 
+# --- SUPPORT LINKS ---
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/heroku_club")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NOBITA_SUPPORT")
 
-# Spotify Client.. Get it from https://developer.spotify.com/dashboard
+# --- ASSISTANT SETTINGS ---
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", True)
+AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", 1800))
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", False)
+
+# --- DOWNLOAD SLEEP TIME ---
+YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", 3))
+TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", 5))
+
+# --- SPOTIFY SETTINGS ---
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "a29b0b331adf4c428ce3a73a9b20a306")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "e463dede19b04610ad27dc43eb19b56e")
 
-
-# Maximum number of video calls allowed on bot. You can later set it via /set_video_limit on telegram
+# --- FILE SIZE LIMITS ---
 VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", 999))
-
-
-# Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", 500))
-
-# MaximuM limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 500))
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600)) 
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
-
-# Telegram audio and video file size limit (in bytes)
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))  # Remember to give value in bytes
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496)) # Remember to give value in bytes
-
-# Chceckout https://www.gbmb.org/mb-to-bytes  for converting mb to bytes
-
-
-# If you want your bot to setup the commands automatically in the bot's menu set it to true.
-# Refer to https://i.postimg.cc/Bbg3LQTG/image.png
+# --- COMMANDS AUTO-SETUP ---
 SET_CMDS = getenv("SET_CMDS", False)
 
-
-# You'll need a Pyrogram String Session for these vars. Generate String from our session generator bot @ChampuStringBot
+# --- STRING SESSIONS ---
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
-
-
-#   ░█████╗░██╗░░██╗░█████╗░███╗░░░███╗██████╗░██╗░░░██╗
-#   ██╔══██╗██║░░██║██╔══██╗████╗░████║██╔══██╗██║░░░██║
-#   ██║░░╚═╝███████║███████║██╔████╔██║██████╔╝██║░░░██║
-#   ██║░░██╗██╔══██║██╔══██║██║╚██╔╝██║██╔═══╝░██║░░░██║
-#   ╚█████╔╝██║░░██║██║░░██║██║░╚═╝░██║██║░░░░░╚██████╔╝
-#   ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░░░░░╚═════╝░     
-
-
-
-### DONT TOUCH or EDIT codes after this line
+# --- INTERNAL VARIABLES (DO NOT TOUCH) ---
 BANNED_USERS = filters.user()
 YTDOWNLOADER = 1
 LOG = 2
@@ -163,181 +98,51 @@ votemode = {}
 confirmer = {}
 userstats = {}
 clean = {}
-
 autoclean = []
 
+# --- IMAGE URLS (Yahan aapki saari images hain) ---
+# Default Link: https://files.catbox.moe/uohequ.jpg
 
-# Images
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+PLAYLIST_IMG_URL = getenv("PLAYLIST_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+GLOBAL_IMG_URL = getenv("GLOBAL_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+STATS_IMG_URL = getenv("STATS_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+TELEGRAM_AUDIO_URL = getenv("TELEGRAM_AUDIO_URL", "https://files.catbox.moe/uohequ.jpg")
+TELEGRAM_VIDEO_URL = getenv("TELEGRAM_VIDEO_URL", "https://files.catbox.moe/uohequ.jpg")
+STREAM_IMG_URL = getenv("STREAM_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+SOUNCLOUD_IMG_URL = getenv("SOUNCLOUD_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+YOUTUBE_IMG_URL = getenv("YOUTUBE_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+SPOTIFY_ARTIST_IMG_URL = getenv("SPOTIFY_ARTIST_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+SPOTIFY_ALBUM_IMG_URL = getenv("SPOTIFY_ALBUM_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
+SPOTIFY_PLAYLIST_IMG_URL = getenv("SPOTIFY_PLAYLIST_IMG_URL", "https://files.catbox.moe/uohequ.jpg")
 
-START_IMG_URL = getenv(
-    "START_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-PING_IMG_URL = getenv(
-    "PING_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-PLAYLIST_IMG_URL = getenv(
-    "PLAYLIST_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-GLOBAL_IMG_URL = getenv(
-    "GLOBAL_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-STATS_IMG_URL = getenv(
-    "STATS_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-TELEGRAM_AUDIO_URL = getenv(
-    "TELEGRAM_AUDIO_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-TELEGRAM_VIDEO_URL = getenv(
-    "TELEGRAM_VIDEO_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-STREAM_IMG_URL = getenv(
-    "STREAM_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-SOUNCLOUD_IMG_URL = getenv(
-    "SOUNCLOUD_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-YOUTUBE_IMG_URL = getenv(
-    "YOUTUBE_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-SPOTIFY_ARTIST_IMG_URL = getenv(
-    "SPOTIFY_ARTIST_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-SPOTIFY_ALBUM_IMG_URL = getenv(
-    "SPOTIFY_ALBUM_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-SPOTIFY_PLAYLIST_IMG_URL = getenv(
-    "SPOTIFY_PLAYLIST_IMG_URL",
-    "https://files.catbox.moe/uohequ.jpg",
-)
-
-
+# --- HELPER FUNCTIONS ---
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
 
-
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 SONG_DOWNLOAD_DURATION_LIMIT = int(time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00"))
 
-if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
-        print(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
-        )
+# --- URL VALIDATION LOGIC ---
+# Yeh check karega ki aapne links sahi daale hain ya nahi
+def check_url(name, url):
+    if url and url != "https://files.catbox.moe/uohequ.jpg":
+        if not re.match("(?:http|https)://", url):
+            print(f"[ERROR] - Aapka {name} link galat hai! Please check karein.")
 
-
-if SUPPORT_GROUP:
-    if not re.match("(?:http|https)://", SUPPORT_GROUP):
-        print(
-            "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
-        )
-
-
-if UPSTREAM_REPO:
-    if not re.match("(?:http|https)://", UPSTREAM_REPO):
-        print(
-            "[ERROR] - Your UPSTREAM_REPO url is wrong. Please ensure that it starts with https://"
-        )
-
-
-if GITHUB_REPO:
-    if not re.match("(?:http|https)://", GITHUB_REPO):
-        print(
-            "[ERROR] - Your GITHUB_REPO url is wrong. Please ensure that it starts with https://"
-        )
-
-
-if PING_IMG_URL:
-    if PING_IMG_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", PING_IMG_URL):
-            print(
-                "[ERROR] - Your PING_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-
-
-if PLAYLIST_IMG_URL:
-    if PLAYLIST_IMG_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", PLAYLIST_IMG_URL):
-            print(
-                "[ERROR] - Your PLAYLIST_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-
-
-if GLOBAL_IMG_URL:
-    if GLOBAL_IMG_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", GLOBAL_IMG_URL):
-            print(
-                "[ERROR] - Your GLOBAL_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-
-
-if STATS_IMG_URL:
-    if STATS_IMG_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", STATS_IMG_URL):
-            print(
-                "[ERROR] - Your STATS_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-
-
-if TELEGRAM_AUDIO_URL:
-    if TELEGRAM_AUDIO_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", TELEGRAM_AUDIO_URL):
-            print(
-                "[ERROR] - Your TELEGRAM_AUDIO_URL url is wrong. Please ensure that it starts with https://"
-            )
-
-
-if STREAM_IMG_URL:
-    if STREAM_IMG_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", STREAM_IMG_URL):
-            print(
-                "[ERROR] - Your STREAM_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-
-
-if SOUNCLOUD_IMG_URL:
-    if SOUNCLOUD_IMG_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", SOUNCLOUD_IMG_URL):
-            print(
-                "[ERROR] - Your SOUNCLOUD_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-
-
-if YOUTUBE_IMG_URL:
-    if YOUTUBE_IMG_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
-            print(
-                "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-
-
-if TELEGRAM_VIDEO_URL:
-    if TELEGRAM_VIDEO_URL != "https://files.catbox.moe/uohequ.jpg":
-        if not re.match("(?:http|https)://", TELEGRAM_VIDEO_URL):
-            print(
-                "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
-            )
+# Saare Links ko bari-bari check karna
+check_url("SUPPORT_CHANNEL", SUPPORT_CHANNEL)
+check_url("SUPPORT_GROUP", SUPPORT_GROUP)
+check_url("UPSTREAM_REPO", UPSTREAM_REPO)
+check_url("GITHUB_REPO", GITHUB_REPO)
+check_url("PING_IMG_URL", PING_IMG_URL)
+check_url("PLAYLIST_IMG_URL", PLAYLIST_IMG_URL)
+check_url("GLOBAL_IMG_URL", GLOBAL_IMG_URL)
+check_url("STATS_IMG_URL", STATS_IMG_URL)
+check_url("TELEGRAM_AUDIO_URL", TELEGRAM_AUDIO_URL)
+check_url("STREAM_IMG_URL", STREAM_IMG_URL)
+check_url("SOUNCLOUD_IMG_URL", SOUNCLOUD_IMG_URL)
+check_url("YOUTUBE_IMG_URL", YOUTUBE_IMG_URL)
+check_url("TELEGRAM_VIDEO_URL", TELEGRAM_VIDEO_URL)
